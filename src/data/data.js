c3cdc7d1dba7688a1406170f5871df5b55bf1ce8
fahadsaleem164 <igz.iwd.25@gmail.com>
2021-06-23 21:23:22 -0700

    'add'

---
+++ src/data/data.js
@@ -0,0 +1,24 @@
+module.exports = {
+  SiteTitle: 'Abdelali',
+  Sitelogo: '#',
+  SiteLogoText: 'Abdelali',
+  SiteAuthor: 'Abdelali Dahir',
+  SiteDescription: 'Front End Developer',
+  defaultDescription: 'Software engineer!', 
+  SiteSocialLinks: {
+    twitter: 'https://twitter.com/AbdaliDahir',
+    github: 'https://github.com/AbdaliDahir',
+    linkedin: 'https://www.linkedin.com/in/abdelali-dahir-0b5344111/',
+  },
+  SiteAddress: {
+    city: 'Casablanca',
+    region: 'CurvaSud',
+    country: 'Morocco',
+    zipCode: 'ZipCode',
+  },
+  SiteContact: {
+    email: 'abdali.dahir@gmail.com',
+    phone: 'phone number',
+  },
+  SiteCopyright: '2020',
+};
